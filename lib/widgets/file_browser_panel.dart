d47e412ddaa64e5706ed6b1c1600b478fa18f489
Accelerator <vvvivyvv@gmail.com>
2026-07-07 19:15:49 +0800

    Revert "fix: WebP/误标扩展名动图也受 GifDisplayMode 控制"
    This reverts commit 24ff0a55e3892870930cb2e21353684b35a14250.

--- lib/widgets/file_browser_panel.dart
+++ lib/widgets/file_browser_panel.dart
@@ -402,7 +402,7 @@ class _FileGridItemState extends State<_FileGridItem> {
     final isDir = widget.file is Directory;
     final isImage = !isDir &&
         previewExtensions.any((ext) => name.toLowerCase().endsWith(ext));
-    final isGif = !isDir && GifImage.isAnimatedFile(widget.file.path);
+    final isGif = !isDir && name.toLowerCase().endsWith('.gif');
 
     return MouseRegion(
       onEnter: (_) => setState(() => _isHovering = true),
